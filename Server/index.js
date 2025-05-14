be5196fff366cceddbbd2168f349519cb80177eb
Muhammad-Usama128 <mudeveloper1@gmail.com>
2025-05-14 19:38:34 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const connection = require("./config");
+const connection = require("./config");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const app = express();
