9cdf1771b2f05b6cdde067456a0692c78756cba8
Muhammad-Usama128 <mudeveloper1@gmail.com>
2025-05-16 18:32:22 +0500

    Create index.js

---
+++ Server/api/index.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    return res.status(200).json({ message: "Email already exists" });
+  } else {
+    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+}
