956d0c09c2d9e6306cc178c9bd851c123bdd277f
Muhammad-Usama128 <mudeveloper1@gmail.com>
2025-05-16 19:21:39 +0500

    Update register.js

--- Server/api/register.js
+++ Server/api/register.js
@@ -33,17 +33,18 @@
 import dbConnect from "../lib/mongodb";
 
 export default async function handler(req, res) {
-  // Handle preflight OPTIONS request
+  // Set CORS headers for all requests
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight (OPTIONS) request
   if (req.method === "OPTIONS") {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-    res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     return res.status(200).end();
   }
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
   await dbConnect();
+
   const { namedata, emaildata, passworddata } = req.body;
 
   try {
