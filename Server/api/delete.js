47c9a517c36b6f2dc83c35cfdddcaf288442199e
Muhammad-Usama128 <mudeveloper1@gmail.com>
2025-05-16 20:24:27 +0500

    Update delete.js

--- Server/api/delete.js
+++ Server/api/delete.js
@@ -1,20 +1,19 @@
 // api/delete.js
-import dbConnect from "../../lib/mongodb";
-import mongoose from "mongoose";
+import { dbConnect, User } from "../../lib/mongodb";
+import allowCors from "../../lib/cors";
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).end();
 
   await dbConnect();
-  const connection = mongoose.connection.collection("accounts");
-
   const { id, email } = req.body;
 
   try {
-    await connection.updateOne({ email }, { $pull: { posts: { _id: id } } });
-
+    await User.updateOne({ email }, { $pull: { posts: { _id: id } } });
     res.status(200).json({ message: "Deleted Successfully" });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error });
+    res.status(500).json({ message: "Server error", error: error.message });
   }
 }
+
+export default allowCors(handler);
