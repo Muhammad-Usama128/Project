dd59b4e087de5f8caa63e3ebf33c2dcc14687627
Muhammad-Usama128 <mudeveloper1@gmail.com>
2025-05-16 20:18:44 +0500

    Update register.js

--- Server/api/register.js
+++ Server/api/register.js
@@ -1,18 +1,18 @@
-import dbConnect from "../lib/mongodb";
+import { dbConnect, User } from "../lib/mongodb";
 import allowCors from "../lib/cors";
 
 async function handler(req, res) {
   await dbConnect();
 
   const { namedata, emaildata, passworddata } = req.body;
+
   try {
-    const collection = await getAccountsCollection();
-    const user = await collection.findOne({ email: emaildata });
+    const user = await User.findOne({ email: emaildata });
 
     if (user) {
       return res.status(200).json({ message: "Email already exists" });
     } else {
-      await collection.insertOne({
+      await User.create({
         name: namedata,
         email: emaildata,
         password: passworddata,
@@ -21,9 +21,8 @@ async function handler(req, res) {
       return res.status(200).json({ message: "Email is available" });
     }
   } catch (error) {
-    console.error("API /register error:", error); 
-    return res.status(500).json({ message: "Server error", error });
-    console.log(error)
+    console.error("API /register error:", error);
+    return res.status(500).json({ message: "Server error", error: error.message });
   }
 }
 
