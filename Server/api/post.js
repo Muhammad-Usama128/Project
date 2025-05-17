d16403724e4057353d91221e10735ff9a7ecdb10
Muhammad-Usama128 <mudeveloper1@gmail.com>
2025-05-17 08:06:22 +0500

    Update post.js

--- Server/api/post.js
+++ Server/api/post.js
@@ -20,6 +20,7 @@ async function handler(req, res) {
     );
     res.status(200).json({ message: "Posted Successfully" });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Server error", error });
   }
 }
